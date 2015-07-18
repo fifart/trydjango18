b1158898e7f2763d4c7846efd6605033619a4cb7
fifart <fifart@gmail.com>
2015-07-18 13:59:54 +0300

    v 14/42

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
+from .forms import SignUpForm
 
 # Create your views here.
 def home(request):
 	title = "Hi"
-	if request.user.is_authenticated():
-		title = "Welcome %s" % (request.user)
+	form = SignUpForm(request.POST or None)
 	context = {
 		"template_title": title,
+		"form":form,
 
 	}
+	if form.is_valid():
+		instance = form.save(commit=False)
+		full_name = form.cleaned_data.get("full_name")
+		instance.full_name = full_name
+		instance.save()
+	context = {
+		"template_title": "Thank You",
+		
+
+	}
+	
 	return render(request, "home.html", context)
